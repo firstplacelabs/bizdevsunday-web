f7815f3327142b3db481114a883ab7175ea0387d
ctaylor <ct@ct-mbp1.local>
2017-07-20 21:24:56 -0400

    remove comments

--- config/development.js
+++ config/development.js
@@ -2,9 +2,6 @@
 export default {
   cosmicjs: {
     bucket: {
-      //slug: process.env.COSMIC_BUCKET || 'cross-platform-blog',
-      //read_key: process.env.COSMIC_READ_KEY,
-      //write_key: process.env.COSMIC_WRITE_KEY
       slug: 'bizdevsunday',
       read_key: 'bw5i2nxtaJh7lwfZKJtyFAR5LJIf0w7KCyiVbgWYMFgkM5Bov7',
       write_key: 'CbEIX0l0yLSWx128UCNgVCGIQFD4DIbhCHuJTR8p6dlhQFQHpC'
